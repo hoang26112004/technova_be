0baa0f96ad089290f153321e0c3b0b509b473ff6
HOANG <hoang261104@gmail.com>
2026-03-11 21:53:49 +0700

    Initial commit

---
+++ src/main/java/com/example/technova_be/TechnovaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.technova_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnovaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnovaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/technova_be/TechnovaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technova_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnovaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
